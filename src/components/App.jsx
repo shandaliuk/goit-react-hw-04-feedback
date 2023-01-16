ebf8ef72ecfc72a6e51cabd1fd910c63897deea4
Kyrylo Shandaliuk <shandaliuk3019@gmail.com>
2023-01-16 19:46:55 +0200

    Swapped useReducer with useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,55 @@
-import { useReducer } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-const initialState = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
-
-const feedbackReducer = (state, action) => {
-  switch (action.type) {
-    case 'good':
-      return { ...state, good: state.good + 1 };
-    case 'neutral':
-      return { ...state, neutral: state.neutral + 1 };
-    case 'bad':
-      return { ...state, bad: state.bad + 1 };
-    default:
-      return new Error('Unknown feedback');
-  }
-};
-
 export const App = () => {
-  const [feedback, dispatch] = useReducer(feedbackReducer, initialState);
+  const [goodFeedbackCount, setGoodFeedbackCount] = useState(0);
+  const [neutralFeedbackCount, setNeutralFeedbackCount] = useState(0);
+  const [badFeedbackCount, setBadFeedbackCount] = useState(0);
 
-  const countTotalFeedback = () => {
-    return Object.values(feedback).reduce((acc, vote) => acc + vote);
-  };
+  const countTotalFeedback = () =>
+    goodFeedbackCount + neutralFeedbackCount + badFeedbackCount;
 
   const countPositiveFeedbackPercentage = () => {
     const total = countTotalFeedback();
     if (total === 0) {
       return total;
     }
-    return Math.round((feedback.good / total) * 100);
+    return Math.round((goodFeedbackCount / total) * 100);
   };
 
-  const onFeedback = name => dispatch({ type: name });
+  const onFeedback = name => {
+    switch (name) {
+      case 'good':
+        setGoodFeedbackCount(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutralFeedbackCount(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBadFeedbackCount(prevState => prevState + 1);
+        break;
+      default:
+        new Error('Unknown feedback');
+    }
+  };
 
   return (
     <>
       <Section title="Please leave your feedback">
         <FeedbackOptions
-          options={Object.keys(feedback)}
+          options={['good', 'neutral', 'bad']}
           onLeaveFeedback={onFeedback}
         />
       </Section>
       <Section title="Statistics">
         {countTotalFeedback() !== 0 ? (
           <Statistics
-            good={feedback.good}
-            neutral={feedback.neutral}
-            bad={feedback.bad}
+            good={goodFeedbackCount}
+            neutral={neutralFeedbackCount}
+            bad={badFeedbackCount}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
